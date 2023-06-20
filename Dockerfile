87f6d9d25dfd93dc3c5fe783a43bbfce7f1c6d1d
Karol Baraniecki <karol@baraniecki.eu>
2023-06-20 13:45:08 +0200

    Vacuum the database after each run

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM alpine:3.18.2
 
+RUN apk add --no-cache sqlite
+
 ADD run.sh linux-build/apifier linux-build/fetcher linux-build/uploader /opt/top-of-github/
 
 WORKDIR /opt/top-of-github
--- run.sh
+++ run.sh
@@ -11,18 +11,24 @@ set +a
 mkdir -p state
 
 while true; do
-        log "fetcher: starting"
-        ./fetcher
-        log "fetcher: ended with code $?"
+	log "fetcher: starting"
+	./fetcher
+	log "fetcher: ended with code $?"
 
-        rm -rfv to-upload
+	rm -rfv to-upload
 
-        log "apifier: starting"
-        ./apifier -output-dir to-upload
-        log "apifier: ended with code $?"
+	log "apifier: starting"
+	./apifier -output-dir to-upload
+	log "apifier: ended with code $?"
 
-        log "uploader: starting"
-        ./uploader -directory to-upload
-        log "uploader: ended with code $?"
+	log "uploader: starting"
+	./uploader -directory to-upload
+	log "uploader: ended with code $?"
+
+	rm -rfv to-upload
+
+	log "sqlite vacuum: starting"
+	sqlite3 state/repos.db "pragma journal_mode=WAL; pragma temp_store_directory='.'; vacuum"
+	log "sqlite vacuum: ended with code $?"
 done
 
