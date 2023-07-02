44b11eb1f414caaecb18a52915d9a7dae90425ff
Karol Baraniecki <karol@baraniecki.eu>
2023-07-02 21:57:54 +0200

    Explain the statistics a bit better

--- frontend/src/components/Statistics.tsx
+++ frontend/src/components/Statistics.tsx
@@ -9,8 +9,8 @@ export default function Statistics(metadata: MetadataReponse): JSX.Element {
 
     return (
         <div id="statistics" className="my-3 p-3 bg-body rounded shadow-sm">
-            <h3>Statistics</h3>
-            <p>storing information about <b>{repos}</b> repositories with <b>{stars}</b> stars combined</p>
+            <h5>Statistics</h5>
+            <p>Storing information about <b>{repos}</b> repositories with <b>{stars}</b> stars combined - collected every GitHub repository with at least 5 stars.</p>
         </div>
     )
 }
