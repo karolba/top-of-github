43f82756d6f369e849623e1057f77d5fc0bac35e
Karol Baraniecki <karol@baraniecki.eu>
2023-08-20 14:05:53 +0200

    Fetch data from the new URL

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -3,7 +3,7 @@ import { MetadataReponse, ToplistPageResponse } from './apitypes.js';
 // Use the dev-api-server when working locally
 const BASE_API_URI = ['localhost', '127.0.0.1'].includes(document.location.hostname) 
     ? 'http://127.0.0.1:10002'
-    : 'https://top-of-github-api.baraniecki.eu';
+    : 'https://data.git-top-repos.net'
 
 export async function getMetadata(): Promise<MetadataReponse> {
     let response = await fetch(`${BASE_API_URI}/metadata`);
